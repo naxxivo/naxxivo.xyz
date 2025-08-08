a5eadd3401ee05477d85a38b6697c09905e7e6d6
Naxxivo <naxivobetaemail@gmail.com>
2025-08-08 21:32:30 +0600

    yy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./App.tsx",
-    "./index.tsx",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
