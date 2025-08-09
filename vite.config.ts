5c4c7c3644e4b4c021dd93d581e626fb6bb6794d
Naxxivo <naxivobetaemail@gmail.com>
2025-08-09 18:45:59 +0600

    tt

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      publicDir: false,
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
